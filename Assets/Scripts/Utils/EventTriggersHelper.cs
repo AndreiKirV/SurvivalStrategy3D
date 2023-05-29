1f278f53e3ec0f8b31bada1b45415f4e0899172b
AndreiKirV <rjvveyyf@gmail.com>
2023-05-29 19:10:36 +0300

    UI Buildiing Store

--- Assets/Scripts/Utils/EventTriggersHelper.cs
+++ Assets/Scripts/Utils/EventTriggersHelper.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 
 namespace Utils
 {
-    [RequireComponent(typeof(EventTrigger))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class EventTriggersHelper : MonoBehaviour
     {
         public UnityEvent PointerEnter;
@@ -19,6 +19,18 @@ namespace Utils
 
         public void  PointerExitInvoke()
         {
+            PointerExit?.Invoke();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other) 
+        {
+            Debug.Log("Enter");
+            PointerEnter?.Invoke();
+        }
+
+        private void OnTriggerExit2D(Collider2D other) 
+        {
+            Debug.Log("Exit");
             PointerExit?.Invoke();
         }
     }
